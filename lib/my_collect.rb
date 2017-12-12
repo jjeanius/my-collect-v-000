cdc83152d3d00d6da6e32c96a60f2b3278f0f005
Judy McGoldrick <jjeanius@verizon.net>
2017-12-12 01:25:28 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(collection)
+  i = 0
+  collection = []
+  while i < collection.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
-
+my_collect(collection) do |lang|
+  lang.upcase
+end
