6cfafa731fe273107d4d9208ff5ec022536f8b6e
Judy McGoldrick <jjeanius@verizon.net>
2017-12-12 02:06:26 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,6 +9,6 @@ collection = []
 end
 
 collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |language|
-  language.upcase
+my_collect(collection) do |lang|
+  lang.upcase
 end
