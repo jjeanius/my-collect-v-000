37d2fd00937b33b8efcaf7a809f0973588889106
Judy McGoldrick <jjeanius@verizon.net>
2017-12-12 02:34:13 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
 def my_collect(collection)
   i = 0
-collections = []
+collection= []
   while i < collection.length
     collection << yield(collection[i])
     i += 1
    end
   collection
 end
+
