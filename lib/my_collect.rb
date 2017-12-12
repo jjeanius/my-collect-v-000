111d8f582ffd96c5a309117f872c7ce79ebde144
Judy McGoldrick <jjeanius@verizon.net>
2017-12-12 01:51:50 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(languages)
   i = 0
   languages = []
   while i < languages.length
-    collection << yield(array[i])
+    collection << yield(languages[i])
     i += 1
    end
   my_collect
